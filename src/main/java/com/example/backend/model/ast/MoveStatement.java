24e6a5136e1b042254a2c4e940c12895fb0973c6
natpakanchamp <champnatpakan@gmail.com>
2026-03-03 19:49:51 +0700

    changed dependency

--- src/main/java/com/example/backend/model/ast/MoveStatement.java
+++ src/main/java/com/example/backend/model/ast/MoveStatement.java
@@ -14,10 +14,8 @@ public record MoveStatement(String directionStr) implements Statement {
 
     @Override
     public void execute(Map<String, Long> localVars, Map<String, Long> globalVars) throws EvalError {
-        // ขั้นตอน 1: Adapt ข้อมูล
         DIRECTION dir = adapter.adapt(directionStr);
 
-        // ขั้นตอน 2: Business Logic
         GameState.pay(1L);
         GameState.move(dir.name());
     }
